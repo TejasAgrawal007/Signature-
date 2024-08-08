b7ed2128342e082efc57a07ea03eff94629bda85
Tejas Agrawal <72118095+TejasAgrawal007@users.noreply.github.com>
2024-08-08 13:33:36 +0530

    Update Features

--- JS/app.js
+++ JS/app.js
@@ -31,3 +31,33 @@ canvas.addEventListener("mousemove", (e) => {
     lastY = event.offsetY;
   }
 });
+
+
+canvas.addEventListener('mouseup', () => {
+  isDrawaing = false
+})
+
+canvaColor.addEventListener('change', (e) => {
+  ctx.fillStyle = e.target.value
+  ctx.fillRect(0,0,600, 450)
+})
+
+fontSize.addEventListener('change', (e) => {
+  ctx.lineWidth = e.target.value
+})
+
+clearBtn.addEventListener('click', (e) => {
+  ctx.clearRect(0,0, canvas.width, canvas.height)
+})
+
+saveBtn.addEventListener('click', () => {
+  localStorage.setItem('convasContexts', canvas.toDataURL());
+
+  let link = document.createElement('a');
+
+  link.download = 'my-canva.png';
+
+  link.href = canvas.toDataURL();
+
+  link.click();
+})
